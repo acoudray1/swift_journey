b0160533440530f5e1a9ef87d01eb59933a30a16
axel COUDRAY <axel.hpm.coudray@gmail.com>
2020-01-29 16:09:19 +0100

    init example #2

---
+++ StackViewDemo2/StackViewDemo2/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  StackViewDemo2
+//
+//  Created by Axel COUDRAY on 29/01/2020.
+//  Copyright © 2020 Axel COUDRAY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    view.addBackground(imageName: "background")
+  }
+  
+  
+}
+
+extension UIView {
+    func addBackground(imageName: String = "YOUR DEFAULT IMAGE NAME", contentMode: UIView.ContentMode = .scaleToFill) {
+        // setup the UIImageView
+        let backgroundImageView = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImageView.image = UIImage(named: imageName)
+        backgroundImageView.contentMode = contentMode
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+
+        addSubview(backgroundImageView)
+        sendSubviewToBack(backgroundImageView)
+
+        // adding NSLayoutConstraints
+        let leadingConstraint = NSLayoutConstraint(item: backgroundImageView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1.0, constant: 0.0)
+        let trailingConstraint = NSLayoutConstraint(item: backgroundImageView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1.0, constant: 0.0)
+        let topConstraint = NSLayoutConstraint(item: backgroundImageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 0.0)
+        let bottomConstraint = NSLayoutConstraint(item: backgroundImageView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0.0)
+
+        NSLayoutConstraint.activate([leadingConstraint, trailingConstraint, topConstraint, bottomConstraint])
+    }
+}
